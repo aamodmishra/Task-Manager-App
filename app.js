a3e342b918736bfcebbbea61ed8275f004808969
amod <119671543+aamodmishra@users.noreply.github.com>
2025-01-09 16:54:26 +0545

    task

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const taskform=document.getElementById("task-form")
-const tasklist=document.getElementById("task-list")
-  
+const taskList=document.getElementById("task-list")
+let taskIndex=1;
 
 taskform.addEventListener("submit",function(event)
 {
@@ -8,6 +8,28 @@ taskform.addEventListener("submit",function(event)
     const taskInput=document.getElementById("task-input");
     const taskText=taskInput.value.trim();
     // console.log(taskText)
-    taskText.innerhtml=document.getElementById("task-list")
+    taskText.innerhtml=document.getElementById("task-list");
+     
+    if (taskText!==""){
+        //create new task item
+        const taskItem =document.createElement("li");
+        taskItem.classList.add("task-item");
+        taskItem.textContent=`${taskIndex}- ${taskText}`;
+        
+        taskItem.addEventListener("click",function(){
+        
+            this.classList.toggle("completed")//this completed can be use as class from css
+           
+        }
+    )
+         // append task item to the task list 
+        taskList.appendChild(taskItem)
+        //increment the task
+        taskIndex++;
+
+
+        //clearing the task form
+        taskInput.value="";
+    }
 }
 )
